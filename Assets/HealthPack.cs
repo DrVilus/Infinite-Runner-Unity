0d3efac0ee5a9edc11413d2906ddaa2048742435
DrVilus <DrVilus@users.noreply.github.com>
2021-01-05 22:58:08 +0700

    healthpack

--- Assets/HealthPack.cs
+++ Assets/HealthPack.cs
@@ -7,12 +7,35 @@ public class HealthPack : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float step = GlobalSettings.speed * Time.deltaTime;
+        transform.Translate(Vector2.left * step);
+        if (transform.position.x < -20)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            GlobalSettings.score += 10;
+            if (GlobalSettings.currentHealth < GlobalSettings.maxHealth)
+            {
+                GlobalSettings.currentHealth += 150;
+                if (GlobalSettings.currentHealth > GlobalSettings.maxHealth)
+                {
+                    GlobalSettings.currentHealth = GlobalSettings.maxHealth;
+                }
+            }
+
+        }
     }
 }
--- Assets/Scripts/Crystal.cs
+++ Assets/Scripts/Crystal.cs
@@ -7,36 +7,40 @@ public class Crystal : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float step = GlobalSettings.speed*Time.deltaTime;
-        transform.Translate(Vector2.left*step);
-        if (transform.position.x < -20){
+        float step = GlobalSettings.speed * Time.deltaTime;
+        transform.Translate(Vector2.left * step);
+        if (transform.position.x < -20)
+        {
             Destroy(gameObject);
-        } 
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player"){
+        if (other.gameObject.tag == "Player")
+        {
             Destroy(this.gameObject);
-            GlobalSettings.score+=15;
+            GlobalSettings.score += 15;
             // if(GlobalSettings.currentHealth < GlobalSettings.maxHealth){
             //     GlobalSettings.currentHealth += 200;
             //}
-                        if(GlobalSettings.currentStamina<GlobalSettings.maxStamina){
-                        GlobalSettings.currentStamina +=10;
-                        if(GlobalSettings.currentStamina>GlobalSettings.maxStamina){
-                        GlobalSettings.currentStamina = GlobalSettings.maxStamina;
-                                               }
-        }
-                        
-        }
+            if (GlobalSettings.currentStamina < GlobalSettings.maxStamina)
+            {
+                GlobalSettings.currentStamina += 10;
+                if (GlobalSettings.currentStamina > GlobalSettings.maxStamina)
+                {
+                    GlobalSettings.currentStamina = GlobalSettings.maxStamina;
+                }
+            }
+
         }
     }
+}
 
-    
+
